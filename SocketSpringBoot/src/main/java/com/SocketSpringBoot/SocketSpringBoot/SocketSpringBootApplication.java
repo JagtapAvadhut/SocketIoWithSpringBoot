10d55ba7d13aecd690b0baf0fe1359cee54e6cc9
JagtapAvadhut <avadhut.pvt@gmail.com>
2024-03-10 21:41:49 +0530

    inside SocketSpringBootApplication.java class added //Documentation 1),2),3)

--- SocketSpringBoot/src/main/java/com/SocketSpringBoot/SocketSpringBoot/SocketSpringBootApplication.java
+++ SocketSpringBoot/src/main/java/com/SocketSpringBoot/SocketSpringBoot/SocketSpringBootApplication.java
@@ -9,5 +9,90 @@ public class SocketSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SocketSpringBootApplication.class, args);
 	}
+//1).
+	//Documentation
+	//Add description
+	//
+	//·
+	//﻿
+	//
+	//Settings
+	//Client version
+	//v2
+	//
+	//Client version that will be used for connecting with the server.
+	//
+	//Handshake path
+	///socket.io
+	//
+	//Server path that will be used during the handshake request.
+	//
+	//Events
+	//avadhut
+	//messageSendToUser
+	//New message
+	//json
+	//{
+	//  "senderName": "avadhut",
+	//  "targetUserName": "pawan",
+	//  "message": ""
+	//}
 
+	//2).
+	//Documentation
+	//Add description
+	//
+	//·
+	//﻿
+	//
+	//Settings
+	//Client version
+	//v2
+	//
+	//Client version that will be used for connecting with the server.
+	//
+	//Handshake path
+	///socket.io
+	//
+	//Server path that will be used during the handshake request.
+	//
+	//Events
+	//pawan
+	//messageSendToUser
+	//New message
+	//json
+	//{
+	//  "senderName": "pawan",
+	//  "targetUserName": "avadhut",
+	//  "message": ""
+	//}
+
+	//3).
+	//Documentation
+	//Add description
+	//
+	//·
+	//﻿
+	//
+	//Settings
+	//Client version
+	//v2
+	//
+	//Client version that will be used for connecting with the server.
+	//
+	//Handshake path
+	///socket.io
+	//
+	//Server path that will be used during the handshake request.
+	//
+	//Events
+	//Devendra
+	//messageSendToUser
+	//New message
+	//Text
+	//{
+	//  "senderName": "Devendra",
+	//  "targetUserName": "avadhut",
+	//  "message": "Hello avadhut"
+	//}
 }
